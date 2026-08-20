d5ac6451f30c33467e90f6cdf4426ff0485ae54b
André Bacchi <andre.rossini.bacchi@gmail.com>
2026-08-19 21:22:31 -0300

    refactor: update color constants for background, enemies, players, and walls in config module

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use macroquad::prelude::*;
 
-pub const BG_COLOR: Color = BROWN;
+pub const BG_COLOR: Color = Color::from_hex(0x1a1c23);
 
 pub const AIM_COLOR: Color = WHITE;
 pub const AIM_BASE_LENGTH: f32 = 10.0;
@@ -15,16 +15,16 @@ pub const BULLET_VEL: f32 = 400.0; // pixels-per-second with dt
 
 pub const ENEMY_RADIUS: f32 = 15.0;
 pub const ENEMY_BORDER_THICKNESS: f32 = 3.0;
-pub const ENEMY_COLOR: Color = BLACK;
-pub const ENEMY_BORDER_COLOR: Color = Color::new(1.0, 0.5, 0.5, 1.0); // TODO: hex
+pub const ENEMY_COLOR: Color = Color::from_hex(0x4e9a06);
+pub const ENEMY_BORDER_COLOR: Color = BLACK;
 pub const ENEMY_INITIAL_LIFE: f32 = 100.0;
 
 pub const MIN_ENEMIES_COUNT: usize = 4;
 
 pub const PLAYER_RADIUS: f32 = 15.0;
 pub const PLAYER_BORDER_THICKNESS: f32 = 3.0;
-pub const PLAYER_COLOR: Color = BLACK;
-pub const PLAYER_BORDER_COLOR: Color = WHITE;
+pub const PLAYER_COLOR: Color = Color::from_hex(0xff9933);
+pub const PLAYER_BORDER_COLOR: Color = BLACK;
 pub const PLAYER_BASE_SPEED: f32 = 300.0; // pixels-per-second with dt
 
-pub const WALL_COLOR: Color = Color::from_hex(0x451911);
+pub const WALL_COLOR: Color = Color::from_hex(0x3d4451);
