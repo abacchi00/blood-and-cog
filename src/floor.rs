21d0a7d730b5046a540d33b3475f982f904b1742
André Bacchi <andre.rossini.bacchi@gmail.com>
2026-08-21 17:20:41 -0300

    feat: include variants  and update rendering logic for dynamic floor appearance

--- src/floor.rs
+++ src/floor.rs
@@ -7,7 +7,8 @@ pub struct Floor {
   pub pos: Vec2,
   width: f32,
   height: f32,
-  has_dirt: bool,
+  variant: f32,
+  variant_color: f32,
 }
 
 impl Floor {
@@ -21,7 +22,8 @@ impl Floor {
       pos: Vec2::new(x, y),
       width,
       height,
-      has_dirt: rand::gen_range(0.0, 100.0) < FLOOR_DIRT_CHANCE,
+      variant: rand::gen_range(0.0, 100.0),
+      variant_color: rand::gen_range(0.0, 100.0),
     }
   }
 }
@@ -30,11 +32,32 @@ impl Renderable for Floor {
   fn render(&self) {
     draw_rectangle(self.pos.x, self.pos.y, self.width, self.height, FLOOR_COLOR);
 
-    if self.has_dirt {
-      draw_circle(self.pos.x + self.width/2.0, self.pos.y + self.height/2.0, self.width/4.0, FLOOR_DIRT_COLOR);
-      draw_circle(self.pos.x + self.width/4.0, self.pos.y + self.height/4.0, self.width/6.0, FLOOR_DIRT_COLOR);
-      draw_circle(self.pos.x + self.width/4.0, self.pos.y + self.height/1.5, self.width/8.0, FLOOR_DIRT_COLOR);
-      draw_circle(self.pos.x + self.width/1.5, self.pos.y + self.height/1.6, self.width/7.0, FLOOR_DIRT_COLOR);
+    let color = match self.variant_color {
+      0.0..80.0 => { FLOOR_DIRT_COLOR },
+      80.0..90.0 => { Color::from_hex(0x332e2a) },
+      90.0..=100.0 => { Color::from_hex(0x2a3230) },
+      _ => FLOOR_DIRT_COLOR,
+    };
+
+
+    match self.variant {
+      0.0..5.0 => {
+        draw_circle(self.pos.x + self.width/2.0, self.pos.y + self.height/2.0, self.width/4.0, color);
+        draw_circle(self.pos.x + self.width/4.0, self.pos.y + self.height/4.0, self.width/6.0, color);
+        draw_circle(self.pos.x + self.width/4.0, self.pos.y + self.height/1.5, self.width/8.0, color);
+        draw_circle(self.pos.x + self.width/1.5, self.pos.y + self.height/1.6, self.width/7.0, color);
+      },
+      5.0..10.0 => {
+        draw_circle(self.pos.x + self.width/1.6, self.pos.y + self.height/4.0, self.width/5.0, color);
+        draw_circle(self.pos.x + self.width/2.0, self.pos.y + self.height/1.5, self.width/6.0, color);
+        draw_circle(self.pos.x + self.width/3.0, self.pos.y + self.height/1.6, self.width/4.0, color);
+      },
+      10.0..15.0 => {
+        draw_circle(self.pos.x + self.width/4.6, self.pos.y + self.height/3.0, self.width/5.0, color);
+        draw_circle(self.pos.x + self.width/2.0, self.pos.y + self.height/2.0, self.width/6.0, color);
+        draw_circle(self.pos.x + self.width/3.0, self.pos.y + self.height/1.2, self.width/6.0, color);
+      }
+       _ => ()
     }
   }
 }
