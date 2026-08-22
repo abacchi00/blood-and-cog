6a702ce06638f11a043ed79ca2cfc1ebcea00077
André Bacchi <andre.rossini.bacchi@gmail.com>
2026-08-21 21:12:07 -0300

    feat: add BorderedRect struct and enhance Wall rendering with brick patterns

--- src/config.rs
+++ src/config.rs
@@ -10,6 +10,7 @@ pub const FLOOR_DIRT_COLOR: Color = Color::from_hex(0x292f40);
 
 // Wall
 pub const WALL_COLOR: Color = Color::from_hex(0x3d4451);
+pub const WALL_BRICKS_COLOR: Color = Color::from_hex(0x4d5563);
 
 // Aim
 pub const AIM_COLOR: Color = WHITE;
--- src/wall.rs
+++ src/wall.rs
@@ -1,15 +1,17 @@
 use macroquad::prelude::*;
 
+use crate::config::*;
 use crate::traits::{
   Renderable,
   Collidable, CollisionShape,
 };
-use crate::config::*;
+use crate::alt_shapes::BorderedRect;
 
 pub struct Wall {
   pub pos: Vec2,
   width: f32,
   height: f32,
+  variant: f32,
 }
 
 impl Wall {
@@ -18,13 +20,66 @@ impl Wall {
       pos: Vec2::new(x, y),
       width: GRID_CELL_SIZE,
       height: GRID_CELL_SIZE,
+      variant: rand::gen_range(0.0, 100.0),
     }
   }
 }
 
 impl Renderable for Wall {
   fn render(&self) {
+    let brick_w: f32 = self.width / 3.0;
+    let brick_h: f32 = self.height / 6.0;
+    
     draw_rectangle(self.pos.x, self.pos.y, self.width, self.height, WALL_COLOR);
+
+    if self.variant < 20.0 {
+      BorderedRect {
+        x: self.pos.x + brick_w * 0.5,
+        y: self.pos.y + brick_h,
+        w: brick_w,
+        h: brick_h,
+        color: WALL_COLOR,
+        b_thick: 1.0,
+        b_color: WALL_BRICKS_COLOR,
+      }.draw();
+      BorderedRect {
+        x: self.pos.x + brick_w,
+        y: self.pos.y + brick_h * 2.0,
+        w: brick_w,
+        h: brick_h,
+        color: WALL_COLOR,
+        b_thick: 1.0,
+        b_color: WALL_BRICKS_COLOR,
+      }.draw();
+      BorderedRect {
+        x: self.pos.x + brick_w * 0.5,
+        y: self.pos.y + brick_h * 3.0,
+        w: brick_w,
+        h: brick_h,
+        color: WALL_COLOR,
+        b_thick: 1.0,
+        b_color: WALL_BRICKS_COLOR,
+      }.draw();
+    } else if self.variant < 40.0 {
+      BorderedRect {
+        x: self.pos.x + brick_w * 0.75,
+        y: self.pos.y + brick_h * 3.0,
+        w: brick_w,
+        h: brick_h,
+        color: WALL_COLOR,
+        b_thick: 1.0,
+        b_color: WALL_BRICKS_COLOR,
+      }.draw();
+      BorderedRect {
+        x: self.pos.x + brick_w * 1.25,
+        y: self.pos.y + brick_h * 4.0,
+        w: brick_w,
+        h: brick_h,
+        color: WALL_COLOR,
+        b_thick: 1.0,
+        b_color: WALL_BRICKS_COLOR,
+      }.draw();
+    }
   }
 }
 
